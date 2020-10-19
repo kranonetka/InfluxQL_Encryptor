e3553abd0ec649b7e4bb4b5acc35858265d41ce7
kranonetka <fakemail4steam@ya.ru>
2020-10-19 13:17:42 +0700

    select_stmt (needs fix); multiple stmts for test

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from parsimonious.grammar import Grammar
 from parsimonious.nodes import NodeVisitor, Node
 from base64 import b64encode
-import random
 
 
 class InfluxQLVisitor(NodeVisitor):
-    def visit_user_name(self, node: Node, visited_children):
-        return b64encode(node.text.encode()).decode()  # Вместо имени пользователя возвращаем base64 от него
+    def encode_token(self, node: Node, visited_children):
+        return f"'{b64encode(node.text.encode()).decode()}'"
     
-    visit_password = visit_user_name  # То же самое для user_name
+    visit_user_name = encode_token
     
-    visit_db_name = visit_user_name  # То же самое для db_name
+    visit_password = encode_token
+    
+    visit_db_name = encode_token
     
     def generic_visit(self, node, visited_children):
         return ''.join(visited_children) or node.text  # Для неопределенных токенов возвращаем просто их текст
@@ -19,12 +20,22 @@ class InfluxQLVisitor(NodeVisitor):
 with open('influxql.grammar', 'r', encoding='utf-8') as fp:  # Считывание грамматики из файла
     influxql_grammar = Grammar(fp.read())  # Чтобы не заморачиваться с экранированием символов
 
-query = """CREATE USER "jdoe" WITH PASSWORD '1337password' WITH ALL PRIVILEGES; DROP DATABASE my_secret_db"""  # Запрос для разбора
+
+queries_for_test = (
+    "DROP DATABASE my_secret_db",
+    "CREATE USER \"jdoe\" WITH PASSWORD '1337password' WITH ALL PRIVILEGES; DROP DATABASE my_secret_db",
+    "SELECT autogen.mymeas FROM python_measurement"
+)
+
 
 if __name__ == '__main__':
-    tree = influxql_grammar.parse(query)  # Получение дерева разбора запроса
-    
-    visitor = InfluxQLVisitor()
-    res = visitor.visit(tree)  # Обход дерева с заменой необходимых токенов
-    print(res)
+    for query in queries_for_test:
+        tree = influxql_grammar.parse(query)  # Получение дерева разбора запроса
+        
+        visitor = InfluxQLVisitor()
+        res = visitor.visit(tree)  # Обход дерева с заменой необходимых токенов
+        print(query)
+        print(res)
+        print()
+    #print(json.dumps(res, indent=1))
 
