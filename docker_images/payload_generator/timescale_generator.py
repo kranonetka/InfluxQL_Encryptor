99a411f2ded44634918b94ea04a4ffeb0d2b9462
Usufjon <yusufjonov98@gmail.com>
2020-11-14 17:03:04 +0700

    [+] Data simulator

--- docker_images/payload_generator/timescale_generator.py
+++ docker_images/payload_generator/timescale_generator.py
@@ -2,6 +2,7 @@ import socket
 
 import psutil
 import psycopg2
+from pgcopy import CopyManager
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 HOST = '127.0.0.1'
@@ -17,7 +18,6 @@ def create_db():
     cur = conn.cursor()
     cur.execute(f"DROP DATABASE IF EXISTS {DATABASE}")
     cur.execute(f"CREATE DATABASE {DATABASE};")
-    cur.execute(f"")
     conn.commit()
     cur.close()
 
@@ -41,9 +41,31 @@ def create_table():
     cur.close()
 
 
+def simulate_data():
+    conn = psycopg2.connect(host=HOST, port=PORT, user=USERNAME, password=PASSWORD, dbname=DATABASE)
+    cur = conn.cursor()
+
+    simulate_query = """SELECT  generate_series(now() - interval '24 hour', now(), interval '5 minute') AS time,
+        md5(random()::text) as hostname,
+        random()*100 AS cpu_percent,
+        random()*10000 AS cpu_freq,
+        (random()*100000000)::int AS memory_used
+        """
+    cur.execute(simulate_query)
+    values = cur.fetchall()
+    cols = ('time', 'hostname', 'cpu_percent', 'cpu_freq', 'memory_used')
+    mgr = CopyManager(conn, 'laptop_meas', cols)
+    mgr.copy(values)
+    conn.commit()
+    cur.close()
+
+
 def main():
     create_db()
     create_table()
+
+    # simulate_data()
+
     conn = psycopg2.connect(host=HOST, port=PORT, user=USERNAME, password=PASSWORD, dbname=DATABASE)
     cur = conn.cursor()
     payload_tpl = "INSERT INTO laptop_meas (time, hostname, cpu_percent, cpu_freq, memory_used) VALUES (now(), %s, %s, %s, %s);"
@@ -51,7 +73,7 @@ def main():
         cur.execute(payload_tpl, (
             socket.gethostname(), psutil.cpu_percent(), psutil.cpu_freq().current, psutil.virtual_memory().used))
         conn.commit()
-
+    cur.close()
 
 if __name__ == '__main__':
     main()
