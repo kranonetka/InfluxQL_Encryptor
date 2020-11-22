bbb07de14952ddbe3ce8a2a5564921d19a4d090d
kranonetka <fakemail4steam@ya.ru>
2020-11-22 20:43:16 +0700

    fdhdhd

--- docker_images/proxy_service/main.py
+++ docker_images/proxy_service/main.py
@@ -5,9 +5,9 @@ from flask import Flask, request
 
 from encryptors import QueryEncryptor, WriteEncryptor
 
-# for env_var in {'INFLUXDB_HOST', 'INFLUXDB_PORT'}:
-#     if env_var not in os.environ:
-#         raise RuntimeError(f'Missing environment variable {env_var}')
+for env_var in {'INFLUXDB_HOST', 'INFLUXDB_PORT'}:
+    if env_var not in os.environ:
+        raise RuntimeError(f'Missing environment variable {env_var}')
 
 app = Flask(__name__)
 
@@ -37,43 +37,4 @@ def write():
 
 
 if __name__ == '__main__':
-    key = b'\x00' * 32
-    visitor = QueryEncryptor(key=key)
-    
-    query = """SELECT
-    mean("memory_used")
-FROM
-    "laptop_meas"
-WHERE
-    ("hostname" = '694d42a595f2')
-    AND time >= now() - 5m and time <= 1605680700000ms
-GROUP BY
-    time(200ms)
-    fill(null)"""
-    
-#     query = """SELECT
-#     mean("memory_used")
-# FROM
-#     "laptop_meas"
-# WHERE
-#     ("hostname" = '8d89770d7eb1')
-#     AND time >= 1605680400000ms and time <= 1605680700000ms
-# GROUP BY
-#     time(200ms)
-#     fill(null)"""
-#     query = """SELECT
-#         mean("memory_used")
-#     FROM
-#         "laptop_meas"
-#     WHERE
-#         ("hostname" = '8d89770d7eb1')
-#         AND time >= 1605680400000ms and time <= 1605680700000ms
-#     GROUP BY
-#         time(200ms)
-#         fill(null)"""
-    
-    print(query)
-    print(visitor.parse(query))
-    
-    exit()
     app.run(debug=True, host='0.0.0.0', port=os.environ.get('FLASK_PORT'))
