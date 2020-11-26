49aa200bcb0e10c630cfc5fc411921b478b83afe
Usufjon <yusufjonov98@gmail.com>
2020-11-26 18:16:12 +0700

    Write api done(?)

--- docker_images/proxy_service/token_aggregators/write_aggregator.py
+++ docker_images/proxy_service/token_aggregators/write_aggregator.py
@@ -1,4 +1,25 @@
+from datetime import datetime
+
+from parsers import WriteParser
+
+
 class WriteAggregator:
     @staticmethod
-    def assemble(tokens: dict) -> str:
-        raise NotImplementedError()
+    def assemble(tokens: dict) -> tuple:
+        table_name = tokens['measurement']
+        columns = ','.join([*tokens['tags'].keys(), *tokens['fields'].keys()])
+        values = (*tokens['tags'].values(), *tokens['fields'].values())
+        if tokens.get('time'):
+            timestamp = datetime.fromtimestamp(int(tokens.get('time')))
+        else:
+            timestamp = "now()"
+        insert_query = f"INSERT INTO {table_name}({columns},time) VALUES ({'%s,' * len(values)}{timestamp})"
+        return insert_query, values
+
+
+if __name__ == "__main__":
+    payload = 'laptop,host=sever,name=acer memory=10i,freq=100 10'
+    wp = WriteParser()
+    tokens = wp.parse(payload)[0]
+    print(tokens)
+    print(WriteAggregator.assemble(tokens))
