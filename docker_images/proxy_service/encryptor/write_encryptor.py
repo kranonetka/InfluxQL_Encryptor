33ea33b4e0699716189ffe4b98f9262ce733e442
kranonetka <fakemail4steam@ya.ru>
2020-12-02 17:34:53 +0700

    refactor: Write tokens encryptor refactor

--- docker_images/proxy_service/encryptor/write_encryptor.py
+++ docker_images/proxy_service/encryptor/write_encryptor.py
@@ -1,5 +1,3 @@
-from paillier.crypto import encrypt as phe_encrypt
-
 from encryptor import Encryptor
 
 
@@ -15,25 +13,27 @@ class WriteTokensEncryptor(Encryptor):
             encrypted_tag_value = self.encrypt_string(tag_value)
             encrypted["tags"][tag_key] = encrypted_tag_value
         
-        for field_key in tokens.get("fields").keys():
-            field_value = tokens.get("fields").get(field_key)
+        for field_key, field_value in tokens.get("fields", {}).items():
             type_of_value = self.types.get(db, {}).get(tokens.get("measurement")).get(field_key).get("type")
-            operations = self.types.get(db, {}).get(tokens.get("measurement")).get(field_key).get("operations")
+            
             if type_of_value == "string":
                 encrypted_field_value = self.encrypt_string(field_value)
-                encrypted["fields"][field_key] = encrypted_field_value
-            elif type_of_value == "int":
-                if operations[0] == ">":
-                    encrypted_field_value = self._ope_cipher.encrypt(field_value)
-                    encrypted["fields"][field_key] = encrypted_field_value
-                elif operations[0] == "+":
-                    encrypted_field_value = phe_encrypt(self.paillier_pub_key, field_value)
-                    encrypted["fields"][field_key] = encrypted_field_value
-            elif type_of_value == "float":
-                if operations[0] == ">":
-                    encrypted_field_value = self._ope_cipher.encrypt(self.float_to_int(field_value))
-                    encrypted["fields"][field_key] = encrypted_field_value
-                elif operations[0] == "+":
-                    encrypted_field_value = phe_encrypt(self.paillier_pub_key, self.float_to_int(field_value))
-                    encrypted["fields"][field_key] = encrypted_field_value
+            
+            elif type_of_value in ('int', 'float'):
+                if type_of_value == 'float':
+                    field_value = self.float_to_int(field_value)
+                
+                operations = self.types.get(db, {}).get(tokens.get("measurement")).get(field_key).get("operations")
+                
+                if operations[0] == '>':
+                    encrypted_field_value = self.ope_encrypt(field_value)
+                elif operations[0] == '+':
+                    encrypted_field_value = self.phe_encrypt(field_value)
+                else:
+                    raise TypeError(f'Unsupported operation {operations[0]}')
+            else:
+                raise TypeError(f'Unsupported type {type_of_value}')
+            
+            encrypted["fields"][field_key] = encrypted_field_value
+        
         return encrypted
