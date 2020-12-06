755edcea95a9cb2c5cab6a738eae472fe11d0c2f
kranonetka <fakemail4steam@ya.ru>
2020-12-06 14:03:43 +0700

    style: SQL queries reformat

--- docker_images/timescale/002_create_table.sql
+++ docker_images/timescale/002_create_table.sql
@@ -1,7 +1,8 @@
-CREATE TABLE laptop_meas (
-    time TIMESTAMPTZ NOT NULL,
-    hostname VARCHAR(256),
+CREATE TABLE laptop_meas
+(
+    time        TIMESTAMPTZ NOT NULL,
+    hostname    VARCHAR(256),
     cpu_percent NUMERIC,
-    cpu_freq NUMERIC,
+    cpu_freq    NUMERIC,
     memory_used NUMERIC
 )
