01910cda4f7209d4f5aee01e9601fdbf9ee02196
kranonetka <fakemail4steam@ya.ru>
2020-12-06 14:03:43 +0700

    style: SQL queries reformat

--- docker_images/timescale/003_create_aggregate.sql
+++ docker_images/timescale/003_create_aggregate.sql
@@ -1,8 +1,9 @@
-CREATE OR REPLACE FUNCTION mul_mod (state NUMERIC, var NUMERIC, modulus NUMERIC)
-RETURNS NUMERIC
-LANGUAGE SQL
-AS $$
-    SELECT MOD(state*var, modulus*modulus)
+CREATE OR REPLACE FUNCTION mul_mod(state NUMERIC, var NUMERIC, modulus NUMERIC)
+    RETURNS NUMERIC
+    LANGUAGE SQL
+AS
+$$
+SELECT MOD(state * var, modulus * modulus)
 $$;
 
 
@@ -10,4 +11,4 @@ CREATE AGGREGATE phe_sum (NUMERIC, NUMERIC) (
     initcond = '1', -- this is the initial state of type POINT
     stype = NUMERIC, -- this is the type of the state that will be passed between steps
     sfunc = mul_mod
-);
+    );
