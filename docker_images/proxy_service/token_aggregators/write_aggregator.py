cf20d60178ca3d44fd2cf172ee150da6b2ce577d
kranonetka <fakemail4steam@ya.ru>
2020-12-07 22:49:29 +0700

    fix: Escaped SQL generation

--- docker_images/proxy_service/token_aggregators/write_aggregator.py
+++ docker_images/proxy_service/token_aggregators/write_aggregator.py
@@ -1,20 +1,35 @@
+from collections import abc
 from datetime import datetime
+from itertools import chain
+from typing import Tuple, Optional
+
+from psycopg2 import sql
 
 from parsers import WriteParser
 
 
 class WriteAggregator:
     @staticmethod
-    def assemble(tokens: dict) -> tuple:
+    def assemble(tokens: dict) -> Tuple[sql.Composable, Optional[abc.Sequence]]:
         table_name = tokens['measurement']
-        columns = ','.join([*tokens['tags'].keys(), *tokens['fields'].keys()])
-        values = (*tokens['tags'].values(), *tokens['fields'].values())
-        if tokens.get('time'):
-            timestamp = datetime.fromtimestamp(int(tokens.get('time')))
+        
+        columns = map(sql.Identifier, chain(tokens['tags'].keys(), tokens['fields'].keys(), ('time',)))
+        values = map(sql.Literal, chain(tokens['tags'].values(), tokens['fields'].values()))
+        
+        if (utc_timestamp := tokens.get('time')) is not None:
+            time = sql.Literal(datetime.utcfromtimestamp(utc_timestamp))
         else:
-            timestamp = "now()"
-        insert_query = f"INSERT INTO {table_name}({columns},time) VALUES ({'%s,' * len(values)}{timestamp})"
-        return insert_query, values
+            time = sql.SQL('now()')
+        
+        values = chain(values, (time,))
+        
+        query = sql.SQL('INSERT INTO {table_name}({columns}) VALUES ({values})').format(
+            table_name=sql.Identifier(table_name),
+            columns=sql.SQL(',').join(columns),
+            values=sql.SQL(',').join(values)
+        )
+        
+        return query, None
 
 
 if __name__ == "__main__":
